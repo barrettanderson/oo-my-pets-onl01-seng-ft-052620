6a1b81c26f7da1e8f2a84819070dab53073d2939
Barrett Anderson <barrettanderson1989@gmail.com>
2020-06-10 22:38:18 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -25,12 +25,12 @@ class Owner
     self.all.clear
   end
 
-  def buy_cat
-
+  def buy_cat(name)
+    name = Cat.new(name)
   end
 
-  def buy_dog
-
+  def buy_dog(name)
+    name = Dog.new(name)
   end
 
 
